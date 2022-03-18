ca711dbfc2db25843357bdfb38704cf48cb2824a
ChainYo <t.chaigneau.tc@gmail.com>
2022-03-18 17:24:08 +0100

    add interface

---
+++ interface/app.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import streamlit as st 
+
+
+st.set_page_config(layout="centered", page_title="Pizza Requester", page_icon="🍕")
+st.title("🍕 Pizza Requester")
+st.subheader("Make your request and get your pizza delivered!")
+
+input_text = st.text_area("Enter your request here:", "")
+if st.button("Submit"):
+    r = requests.post("http://api/predict", data=json.dumps({"sample": input_text})).json()
+    if r["prediction"] == "True":
+        st.success("🎉 Your pizza will be delivered! 🎉")
+        st.write("Enjoy your 🍕.")
+    else:
+        st.error("Sorry, we could not deliver your pizza. 😔")
+    st.write(r)
